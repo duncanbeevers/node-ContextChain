1aae08f3cc5879bd8d5e5c8a371e04496034e41b
Duncan Beevers <duncan@dweebd.com>
2011-05-18 23:46:12 -0700

    Follow convention set earlier in test for checking supplied arguments

--- test/ContextChain.test.js
+++ test/ContextChain.test.js
@@ -5,17 +5,15 @@ var path = require('path');
 require.paths.push(path.join(__dirname, '../lib'));
 var ContextChain = require('ContextChain');
 
-function topic() {
-  var context = {};
-  return {
-    context: context,
-    chain: new ContextChain(context)
-  };
-}
-
 vows.describe('A Context Chain').addBatch({
   'when initialized': {
-    topic: topic,
+    topic: function() {
+      var context = {};
+      return {
+        context: context,
+        chain: new ContextChain(context)
+      };
+    },
     'should implement arc': function(scenario) {
       var suppliedArgs;
       scenario.context.arc = function(x, y, radius, startAngle, endAngle, anticlockwise) {
@@ -37,29 +35,22 @@ vows.describe('A Context Chain').addBatch({
       assert.strictEqual(scenario.context.globalCompositeOperation, 'source-over');
     },
     'should implement terminator createLinearGradient': function(scenario) {
-      var actualArgs, returnValue = {};
+      var actualArgs, expectedArgs = [ 1, 2, 3, 4 ], returnValue = {};
       scenario.context.createLinearGradient = function() {
-        actualArgs = arguments; return returnValue;
+        actualArgs = [].splice.call(arguments, 0);
+        return returnValue;
       };
       var gradient = scenario.chain.createLinearGradient(1, 2, 3, 4);
-      assert.strictEqual(actualArgs.length, 4);
-      assert.strictEqual(actualArgs[0], 1);
-      assert.strictEqual(actualArgs[1], 2);
-      assert.strictEqual(actualArgs[2], 3);
-      assert.strictEqual(actualArgs[3], 4);
+      assert.deepEqual(actualArgs, expectedArgs);
       assert.strictEqual(gradient, returnValue);
     },
     'should decorate result of terminator createLinearGradient': function(scenario) {
-      var actualArgs, returnValue = {};
+      var actualArgs, expectedArgs = [ 1, 2, 3, 4 ], returnValue = {};
       scenario.context.createLinearGradient = function() {
-        actualArgs = arguments; return returnValue;
+        actualArgs = [].splice.call(arguments, 0);
+        return returnValue;
       };
       var gradient = scenario.chain.createLinearGradient(1, 2, 3, 4, function(g) { g.decorated = true; });
-      assert.strictEqual(actualArgs.length, 4);
-      assert.strictEqual(actualArgs[0], 1);
-      assert.strictEqual(actualArgs[1], 2);
-      assert.strictEqual(actualArgs[2], 3);
-      assert.strictEqual(actualArgs[3], 4);
       assert.strictEqual(gradient, returnValue);
       assert.strictEqual(gradient.decorated, true);
     }
